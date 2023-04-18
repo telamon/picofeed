661313a667d8f766ac2adf58881c385eec3243ac
Tony Ivanov <telamohn@pm.me>
2023-04-18 16:42:30 +0200

    POP-0201: index, append, blocks, keys, implemented

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import { schnorr } from '@noble/curves/secp256k1'
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils'
 import { blake3 } from '@noble/hashes/blake3'
+
+// ------ Utils
 const utf8Encoder = new TextEncoder('utf-8')
 const utf8Decoder = new TextDecoder('utf-8')
 
@@ -20,6 +22,7 @@ export const cmp = (a, b, i = 0) => {
   while (a[i] === b[i++]) if (i === a.length) return true
   return false
 }
+export const cpy = (to, from, offset = 0) => { for (let i = 0; i < from.length; i++) to[offset + i] = from[i]; return to }
 
 // ------ POP-01
 export function signPair () {
@@ -36,9 +39,14 @@ export function getPublicKey (privateKey) {
 }
 
 // ------ POP-02
-export const SizeOfKeySegment = 33 // v0
-export function sizeOfBlockSegment (dLen, genesis = false, phat = false) {
-  if (!Number.isFinite(dLen)) fail('Expected dlen: number')
+export const PIC0 = s2b('PIC0')
+export const fmtKEY = 0b01101010
+export const fmtBLK = 0b00100001
+export const sizeOfKeySegment = 33 // v0
+
+export function sizeOfBlockSegment (dLen, genesis = false) {
+  if (!Number.isFinite(dLen)) throw new Error('Expected dlen: number')
+  const phat = dLen > 65536
   if (!genesis) dLen += 64
   if (phat) dLen += 2
   return dLen + 1 + 2 + 64
@@ -46,21 +54,22 @@ export function sizeOfBlockSegment (dLen, genesis = false, phat = false) {
 
 export function createKeySegment (key, b, offset = 0) {
   au8(b)
-  if (b.length < SizeOfKeySegment) fail('BufferUnderflow')
+  if (b.length < sizeOfKeySegment) throw new Error('BufferUnderflow')
   key = toU8(key, 32)
   for (let i = 0; i < 32; i++) b[offset + 1 + i] = key[i]
-  b[offset] = 0b01101010 // RESV|V0|KEY
-  return b.slice(offset, offset + SizeOfKeySegment)
+  b[offset] = fmtKEY // RESV|V0|KEY
+  return b.slice(offset, offset + sizeOfKeySegment)
 }
 
-export function createBlockSegment (data, sk, psig, buffer, offset = 0, phat = false) {
+export function createBlockSegment (data, sk, psig, buffer, offset = 0) {
   au8(buffer)
   if (typeof data === 'string') data = s2b(data)
+  const phat = data.length > 65536
   const o1 = psig ? 64 : 0
   const o2 = phat ? 4 : 2
-  const bsize = sizeOfBlockSegment(data.length, !psig, phat)
+  const bsize = sizeOfBlockSegment(data.length, !psig)
 
-  if (buffer.length - offset < bsize) fail('BufferUnderflow')
+  if (buffer.length - offset < bsize) throw new Error('BufferUnderflow')
   buffer = buffer.subarray(offset, offset + bsize)
 
   const view = new DataView(buffer.buffer) // Views operate on original buffer
@@ -86,16 +95,16 @@ export function createBlockSegment (data, sk, psig, buffer, offset = 0, phat = f
 export class BlockMapper {
   constructor (buffer, offset = 0) {
     au8(buffer)
-    if ((buffer[offset] & 0b11110001) !== 0b100001) fail('InvalidBlockSegment')
+    if ((buffer[offset] & 0b11110001) !== 0b100001) throw new Error('InvalidBlockSegment')
     this.offset = offset
     this.buffer = buffer.subarray(offset)
-    const szo = 1 + 64 + (this.genesis ? 0 : 64)
+    const szo = offset + 1 + 64 + (this.genesis ? 0 : 64)
     const view = new DataView(
       buffer.slice(szo, szo + (this.phat ? 4 : 2)).buffer
     )
     this._size = this.phat ? view.getUint32(0) : view.getUint16(0)
-    this._blksz = sizeOfBlockSegment(this._size, this.genesis, this.phat)
-    if (this.buffer.length < this._blksz) fail('BufferUnderflow')
+    this._blksz = sizeOfBlockSegment(this._size, this.genesis)
+    if (this.buffer.length < this._blksz) throw new Error('BufferUnderflow')
   }
 
   get fmt () { return this.buffer[0] }
@@ -105,8 +114,9 @@ export class BlockMapper {
   get eoc () { return !!(this.fmt & 0b1000) }
   set eoc (v) { this.fmt = (this.fmt & 0b11110111) | (v ? 0b1000 : 0) }
   get sig () { return this.buffer.subarray(1, 1 + 64) }
+  get id () { return this.sig }
   get psig () {
-    if (this.genesis) fail('GenesisNoParent')
+    if (this.genesis) throw new Error('GenesisNoParent')
     return this.buffer.subarray(65, 65 + 64)
   }
 
@@ -118,7 +128,135 @@ export class BlockMapper {
     return this.buffer.subarray(o, o + this.size)
   }
 
+  get key () { return this._pk }
   verify (pk) {
-    return schnorr.verify(this.sig, mkHash(this.buffer.subarray(65)), pk)
+    const hash = mkHash(this.buffer.subarray(65, this._blksz))
+    const v = schnorr.verify(this.sig, hash, pk)
+    if (v) this._pk = pk
+    return v
   }
+
+  toString () {
+    const key = this.key && b2h(this.key.slice(0, 3))
+    const bhex = b2h(this.sig.slice(0, 4))
+      .replace(/(.{2})/g, '$1 ')
+      .trimEnd()
+    const butf = b2s(this.body.slice(0, 12))
+    const sig = b2h(this.sig.slice(0, 4))
+    const psig = this.genesis
+      ? 'GENESIS'
+      : b2h(this.psig.slice(0, 4))
+    return JSON.stringify({ key, sig, psig, size: this.size, bhex, butf })
+  }
+}
+
+// ------ POP-0201
+export class Feed {
+  static signPair = signPair
+  tail = 0
+  constructor (from = 2048, novalidate = false) {
+    if (isFinite(from) && from > 0) {
+      this._buf = cpy(u8n(from), PIC0)
+      this.tail = 4
+    } else if (from instanceof Uint8Array) {
+      this._buf = from
+      this._index(true, novalidate)
+    } else throw new Error('new accepts number or Uint8Array')
+  }
+
+  _grow (min) { // Invalidates all subarrays.
+    let size = this._buf.length
+    if (min < size) return
+    while (size < min) size = size << 1
+    const arr = u8n(size)
+    this._buf = cpy(arr, this._buf)
+  }
+
+  get buffer () { return this._buf.subarray(0, this.tail) }
+
+  append (data, sk) {
+    if (typeof data === 'string') data = s2b(data)
+    const psig = this.last?.sig
+    const pk = schnorr.getPublicKey(sk)
+    if (!this._c.keys.find(k => cmp(k, pk))) {
+      this._grow((this._buf.length - this.tail) + sizeOfKeySegment)
+      createKeySegment(pk, this._buf, this.tail)
+      this.tail += sizeOfKeySegment
+    }
+    const req = sizeOfBlockSegment(data.length, !psig)
+    this._grow((this._buf.length - this.tail) + req)
+    const b = createBlockSegment(data, sk, psig, this._buf, this.tail)
+    this.tail += b.length
+    return this.length
+  }
+
+  get keys () {
+    this._index()
+    return this._c.keys
+  }
+
+  get last () { return this.block(-1) }
+  get first () { return this.block(0) }
+  get length () { return this.blocks.length }
+  get blocks () {
+    this._index()
+    return this._c.blocks
+  }
+
+  block (n) {
+    const blocks = this.blocks
+    if (n < 0) n = blocks.length + n
+    return blocks[n]
+  }
+
+  _index (reindex = false, novalidate = false) {
+    if (!this._c || reindex) this._c = { keys: [], blocks: [], offset: 0 }
+    const { keys, blocks } = this._c
+    // Skip magic
+    if (!this._c.offset && cmp(this._buf.subarray(0, 4), PIC0)) this._c.offset = 4
+
+    let seg = null
+    while ((seg = nextSegment(this._buf, this._c.offset))) {
+      const { type, key, block, end } = seg
+      switch (type) {
+        case 0: keys.push(key); break // KEY
+        case 1: { // BLK
+          blocks.push(block)
+          if (novalidate) break
+          const p = blocks[blocks.length - 2]
+          if (p && !cmp(p.sig, block.psig)) throw new Error('InvalidParent')
+          for (let i = 0; i < keys.length; i++) if (block.verify(keys[i])) break
+          if (!block.key) throw new Error('InvalidFeed')
+        } break
+        default: return // Stop indexing on unkown byte
+      }
+      if (end > this.tail) this.tail = end
+      this._c.offset = end
+      if (block?.eoc) break // Stop indexing on last block
+    }
+  }
+
+  clone (novalidate = false) {
+    return new Feed(cpy(u8n(this.tail), this.buffer), novalidate)
+  }
+}
+
+function nextSegment (buffer, offset = 0) {
+  if (buffer.length - offset < 33) return null // OEC
+  const fmt = buffer[offset]
+  const type = fmt === 0b01101010
+    ? 0
+    : (fmt & 0b11110001) === 0b00100001 ? 1 : -1
+  const value = { type, block: null, key: null, end: offset }
+  switch (type) {
+    case 0: // KEY
+      value.key = buffer.subarray(offset + 1, offset + 33)
+      value.end += 33
+      break
+    case 1: // BLK
+      value.block = new BlockMapper(buffer, offset)
+      value.end += value.block.blockSize
+      break
+  }
+  return value
 }
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 import { webcrypto } from 'node:crypto'
 import { test } from 'brittle'
 import {
+  Feed,
   signPair,
   createKeySegment,
   createBlockSegment,
@@ -13,25 +14,25 @@ if (!globalThis.crypto) globalThis.crypto = webcrypto
 
 test('POP-02 spec', async t => {
   const { sk, pk } = signPair()
-  console.log('Public', pk.length, pk)
-  console.log('Private', sk.length, sk)
+  // console.log('Public', pk.length, pk)
+  // console.log('Private', sk.length, sk)
 
   const feed = new Uint8Array(1024)
   let offset = 0
   const k0 = createKeySegment(pk, feed)
   offset += k0.length
-  console.log('K0', k0.length, b2h(k0))
+  // console.log('K0', k0.length, b2h(k0))
 
   const b0 = createBlockSegment('hack', sk, null, feed, k0.length)
   offset += b0.length
-  console.log('B0', b0.length, b2h(b0))
+  // console.log('B0', b0.length, b2h(b0))
   const bm0 = new BlockMapper(b0)
   t.is(bm0.eoc, true)
   bm0.eoc = false
 
   const b1 = createBlockSegment('planet', sk, bm0.sig, feed, offset)
   offset += b1.length
-  console.log('B1', b1.length, b2h(b1))
+  // console.log('B1', b1.length, b2h(b1))
   const bm1 = new BlockMapper(b1)
 
   // Final integrity assertion / validate chain
@@ -49,6 +50,37 @@ test('POP-02 spec', async t => {
   t.is(b2h(bm1.psig), b2h(bm0.sig))
   t.is(bm1.verify(pk), true)
   t.is(b2s(bm1.body), 'planet')
-  const f = feed.subarray(0, offset)
-  console.log('Feed:', b2h(f))
+  // console.log('Feed:', b2h(feed.subarray(0, offset)))
+})
+
+test('POP-0201 Feed.new(), append(), blocks(), keys(), clone()', async t => {
+  const feed = new Feed()
+  const { sk, pk } = Feed.signPair()
+
+  const h = feed.append('Hello World', sk)
+  t.is(h, 1)
+  const b0 = feed.block(0)
+  // console.log('BLOCK0', b0.toString())
+  t.is(b2s(b0.body), 'Hello World')
+  t.is(b2h(b0.key), pk)
+  t.ok(feed.last)
+
+  t.is(feed.append('</world>', sk), 2)
+
+  const b1 = feed.block(1)
+  // console.log('BLOCK1', b1.toString())
+  t.is(b2h(b1.psig), b2h(b0.sig))
+  t.is(b2s(b1.body), '</world>')
+
+  t.is(b2h(feed.first.sig), b2h(b0.sig))
+  t.is(feed.blocks.length, 2)
+  t.is(feed.keys.length, 1)
+  t.is(b2h(feed.keys[0]), pk)
+
+  const f2 = feed.clone()
+  t.is(b2h(f2.buffer), b2h(feed.buffer))
+})
+
+test('POP-0201 ', async t => {
+
 })
